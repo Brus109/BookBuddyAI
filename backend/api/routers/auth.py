b9bf87e0ae8f8ce4322953bd66178ffaafdd425f
Brus109 <luised10.52@gmail.com>
2025-09-18 17:15:53 -0600

    correcion en ruta auth para el manejo de registro y login

--- backend/api/routers/auth.py
+++ backend/api/routers/auth.py
@@ -1,22 +1,31 @@
 from fastapi import APIRouter, HTTPException
 from fastapi.security import OAuth2PasswordBearer
 from utils.database import supabase
+from pydantic import BaseModel
+
+class UserRegister(BaseModel):
+    email: str
+    password: str
+
+class UserLogin(BaseModel):
+    email: str
+    password: str
 
 router = APIRouter(prefix="/auth", tags=["Auth"])
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
 
 @router.post("/register")
-async def register(email: str, password: str):
+async def register(user_data: UserRegister):
     try:
-        user = supabase.auth.sign_up({"email": email, "password": password})
+        user = supabase.auth.sign_up({"email": user_data.email, "password": user_data.password})
         return {"message": "User created"}
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
 
 @router.post("/login")
-async def login(email: str, password: str):
+async def login(user_data: UserLogin):
     try:
-        user = supabase.auth.sign_in_with_password({"email": email, "password": password})
+        user = supabase.auth.sign_in_with_password({"email": user_data.email, "password": user_data.password})
         return {"token": user.session.access_token}
-    except Exception as e:
+    except Exception as e:  
         raise HTTPException(status_code=401, detail="Invalid credentials")
--- backend/api/routers/books.py
+++ backend/api/routers/books.py
@@ -1,7 +1,14 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from services.book_service import BookService
+from core.security import get_current_user
 
 router = APIRouter(prefix="/books", tags=["Books"])
 
-@router.get("/test")
-def test_router():
-    return {"message": "¡Router de libros funcionando!"}
+@router.get("/")
+async def list_books(_=Depends(get_current_user)):
+    return "test" 
+    #BookService.get_all_books()
+
+@router.post("/")
+async def add_book(book_data: dict, _=Depends(get_current_user)):
+    return BookService.create_book(book_data)
